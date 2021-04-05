dd791142f151570d92e17d465311ab8560ec31d4
JuanSuppicich <suppicichjuan@gmail.com>
2021-04-05 18:14:11 -0300

    Add Spring Initializr

---
+++ src/main/java/com/durandsuppicich/danmspedidos/DanMsPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.durandsuppicich.danmspedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanMsPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanMsPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/durandsuppicich/danmspedidos/DanMsPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.durandsuppicich.danmspedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanMsPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
