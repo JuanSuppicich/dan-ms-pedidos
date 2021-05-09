1ab56188d8f37daf534a841b976c6ed0d2fffa6d
JuanSuppicich <suppicichjuan@gmail.com>
2021-05-09 15:07:56 -0300

    Format documents

--- src/main/java/com/durandsuppicich/danmspedidos/exception/MensajeError.java
+++ src/main/java/com/durandsuppicich/danmspedidos/exception/MensajeError.java
@@ -48,7 +48,4 @@ public class MensajeError {
         ", timestamp=" + timeStamp + '\n' +
         "}";
     }
-
-    
-
 }
--- src/main/java/com/durandsuppicich/danmspedidos/exception/NotFoundException.java
+++ src/main/java/com/durandsuppicich/danmspedidos/exception/NotFoundException.java
@@ -1,7 +1,7 @@
 package com.durandsuppicich.danmspedidos.exception;
 
 public class NotFoundException extends RuntimeException {
-    
+
     private static final String DESCRIPCION = "Not Found Exception (404)";
 
     public NotFoundException(String detalle) {
