6d2f3e146e57c5b0da54ac1d285969da2ca10d1d
JuanSuppicich <suppicichjuan@gmail.com>
2021-07-24 17:25:47 -0300

    Register service with eureka

--- src/main/java/com/durandsuppicich/danmspedidos/DanMsPedidosApplication.java
+++ src/main/java/com/durandsuppicich/danmspedidos/DanMsPedidosApplication.java
@@ -2,10 +2,12 @@ package com.durandsuppicich.danmspedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@EnableDiscoveryClient
 public class DanMsPedidosApplication {
 
 	public static void main(String[] args) {
