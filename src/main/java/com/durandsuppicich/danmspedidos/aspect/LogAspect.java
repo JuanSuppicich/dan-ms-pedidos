2f79c9065daa93fc69701b994b87a5055e026f25
JuanSuppicich <suppicichjuan@gmail.com>
2021-08-17 20:09:04 -0300

    Add exception and client logs

--- src/main/java/com/durandsuppicich/danmspedidos/aspect/LogAspect.java
+++ src/main/java/com/durandsuppicich/danmspedidos/aspect/LogAspect.java
@@ -23,14 +23,20 @@ public class LogAspect {
     @Pointcut("execution(* com.durandsuppicich.danmspedidos.repository.*.*(..))")
     private void repositoryMethods() {}
 
-    @Before("servicesMethods() || repositoryMethods()" )
+    @Pointcut("execution(* com.durandsuppicich.danmspedidos.exception.*.*(..))")
+    private void exceptions() {}
+
+    @Pointcut("execution(* com.durandsuppicich.danmspedidos.client.*.*(..))")
+    private void client() {}
+
+    @Before("servicesMethods() || repositoryMethods() || exceptions() || client()" )
     public void doBefore(JoinPoint joinPoint) {
         logger.debug(joinPoint.getTarget().getClass() + 
             ". METODO A EJECUTAR: " + joinPoint.getSignature().getName() + 
             ". ARGUMENTOS: " + Arrays.toString(joinPoint.getArgs()));
     }
 
-    @After("servicesMethods() || repositoryMethods()")
+    @After("servicesMethods() || repositoryMethods() || client()")
     public void doAfter(JoinPoint joinPoint) {
         logger.debug(joinPoint.getTarget().getClass() + 
             ". METODO EJECUTADO: " + joinPoint.getSignature().getName() + 
